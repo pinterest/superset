e50eaceaa8171eae69e261e1e2ba12c80bd62e0c
kgopal <kgopal@pinterest.com>
2023-01-30 17:11:50 -0800

    fix name

--- superset/viz.py
+++ superset/viz.py
@@ -305,7 +305,7 @@ class BaseViz:  # pylint: disable=too-many-public-methods
                 timestamp_format=timestamp_format,
                 offset=self.datasource.offset,
                 time_shift=self.time_shift,
-                dttm_col=(granularity_col.column_name if granularity_col else DTTM_ALIAS),
+                dttm_col_name=(granularity_col.column_name if granularity_col else DTTM_ALIAS),
             )
 
             if self.enforce_numerical_metrics:
