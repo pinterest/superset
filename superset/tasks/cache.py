e38da4eba364cc02cce1ca65a05fd0e64142c15c
Krishna Gopal <kgopal@pinterest.com>
2025-01-06 18:28:16 -0600

    Fix python workflows (#41)

--- superset/tasks/cache.py
+++ superset/tasks/cache.py
@@ -32,7 +32,6 @@ from superset.tags.models import Tag, TaggedObject
 from superset.tasks.utils import fetch_csrf_token
 from superset.utils import json
 from superset.utils.date_parser import parse_human_datetime
-from superset.utils.machine_auth import MachineAuthProvider
 from superset.utils.urls import get_url_path
 
 logger = get_task_logger(__name__)
@@ -238,11 +237,10 @@ class DashboardMetadataStrategy(Strategy):  # pylint: disable=too-few-public-met
 
     def get_payloads(self) -> list[dict[str, int]]:
         payloads = []
-        session = db.create_scoped_session()
 
         # add dashboards that have cache warmup configured
         cache_configured_dashboards = (
-            session.query(Dashboard)
+            db.session.query(Dashboard)
             .filter(Dashboard.json_metadata.like('%"cache_warmup_schedule"%'))
             .all()
         )
