a17597665db40242baba0979b812c215e4a12e80
Krishna Gopal <kgopal492@gmail.com>
2025-09-26 13:51:04 +0100

    Remove optionName from chart data cachekey, add option to cache metric values (#104)
    * Remove optionName key from explore data cache, allow column values api to cache column values

--- tests/integration_tests/datasource/api_tests.py
+++ tests/integration_tests/datasource/api_tests.py
@@ -145,6 +145,7 @@ class TestDatasourceApi(SupersetTestCase):
             column_name="col2",
             limit=10000,
             denormalize_column=False,
+            use_cache=False,
         )
 
     @pytest.mark.usefixtures("app_context", "virtual_dataset")
@@ -167,6 +168,7 @@ class TestDatasourceApi(SupersetTestCase):
             column_name="col2",
             limit=10000,
             denormalize_column=True,
+            use_cache=False,
         )
 
     @pytest.mark.usefixtures("app_context", "virtual_dataset")
