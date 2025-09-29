4b4896a1ffdd4b5995d7e5e30eedea1845b9f1f6
Krishna Gopal <kgopal492@gmail.com>
2025-09-29 16:06:24 +0100

    fix lint error (#106)

--- superset-frontend/src/explore/components/ChartPills.tsx
+++ superset-frontend/src/explore/components/ChartPills.tsx
@@ -84,9 +84,10 @@ export const ChartPills = forwardRef(
               0,
           );
 
-    const pinterestChartPills = useMemo(() => {
-      return getPinterestChartPills(isLoading, firstQueryResponse);
-    }, [isLoading, firstQueryResponse]);
+    const pinterestChartPills = useMemo(
+      () => getPinterestChartPills(isLoading, firstQueryResponse),
+      [isLoading, firstQueryResponse],
+    );
 
     return (
       <div ref={ref}>
