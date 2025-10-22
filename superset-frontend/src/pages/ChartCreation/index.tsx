fde4b364572945d894cf6a3c475242edd37159e5
Krishna Gopal <kgopal492@gmail.com>
2025-10-22 13:39:39 +0100

    Implement dataset advanced search modal in chart creation (#113)
    * Implement dataset advanced search modal in chart creation

--- superset-frontend/src/pages/ChartCreation/index.tsx
+++ superset-frontend/src/pages/ChartCreation/index.tsx
@@ -43,10 +43,12 @@ import {
   Dataset,
   DatasetSelectLabel,
 } from 'src/features/datasets/DatasetSelectLabel';
+import DatasourceAdvancedSearchModal from 'src/components/Datasource/DatasourceAdvancedSearchModal';
 
 export interface ChartCreationProps extends RouteComponentProps {
   user: UserWithPermissionsAndRoles;
   addSuccessToast: (arg: string) => void;
+  addDangerToast: (arg: string) => void;
 }
 
 export type ChartCreationState = {
@@ -54,6 +56,7 @@ export type ChartCreationState = {
   datasetName?: string | string[] | null;
   vizType: string | null;
   canCreateDataset: boolean;
+  showAdvancedSearch: boolean;
 };
 
 const ESTIMATED_NAV_HEIGHT = 56;
@@ -90,15 +93,22 @@ const StyledContainer = styled.div`
       flex-direction: row;
       align-items: center;
       margin-bottom: ${theme.gridUnit * 5}px;
+      gap: ${theme.gridUnit * 2}px;
 
-      & > div {
+      & .dataset-select {
+        flex: 1;
         min-width: 200px;
-        width: 300px;
       }
 
-      & > span {
+      & .dataset-actions {
+        display: flex;
+        align-items: center;
+        gap: ${theme.gridUnit * 2}px;
+        flex-shrink: 0;
+      }
+
+      & .help-text {
         color: ${theme.colors.grayscale.light1};
-        margin-left: ${theme.gridUnit * 4}px;
       }
     }
 
@@ -198,6 +208,7 @@ export class ChartCreation extends PureComponent<
         'Dataset',
         props.user.roles,
       ),
+      showAdvancedSearch: false,
     };
 
     this.changeDatasource = this.changeDatasource.bind(this);
@@ -205,6 +216,10 @@ export class ChartCreation extends PureComponent<
     this.gotoSlice = this.gotoSlice.bind(this);
     this.loadDatasources = this.loadDatasources.bind(this);
     this.onVizTypeDoubleClick = this.onVizTypeDoubleClick.bind(this);
+    this.openAdvancedSearch = this.openAdvancedSearch.bind(this);
+    this.closeAdvancedSearch = this.closeAdvancedSearch.bind(this);
+    this.handleAdvancedSearchSelect =
+      this.handleAdvancedSearchSelect.bind(this);
   }
 
   componentDidMount() {
@@ -251,6 +266,18 @@ export class ChartCreation extends PureComponent<
     if (!this.isBtnDisabled()) {
       this.gotoSlice();
     }
+  }
+
+  openAdvancedSearch() {
+    this.setState({ showAdvancedSearch: true });
+  }
+
+  closeAdvancedSearch() {
+    this.setState({ showAdvancedSearch: false });
+  }
+
+  handleAdvancedSearchSelect(datasource: { label: string; value: string }) {
+    this.setState({ datasource });
   }
 
   loadDatasources(search: string, page: number, pageSize: number) {
@@ -332,18 +359,28 @@ export class ChartCreation extends PureComponent<
             status={this.state.datasource?.value ? 'finish' : 'process'}
             description={
               <StyledStepDescription className="dataset">
-                <AsyncSelect
-                  autoFocus
-                  ariaLabel={t('Dataset')}
-                  name="select-datasource"
-                  onChange={this.changeDatasource}
-                  options={this.loadDatasources}
-                  optionFilterProps={['id', 'label']}
-                  placeholder={t('Choose a dataset')}
-                  showSearch
-                  value={this.state.datasource}
-                />
-                {datasetHelpText}
+                <div className="dataset-select">
+                  <AsyncSelect
+                    autoFocus
+                    ariaLabel={t('Dataset')}
+                    name="select-datasource"
+                    onChange={this.changeDatasource}
+                    options={this.loadDatasources}
+                    optionFilterProps={['id', 'label']}
+                    placeholder={t('Choose a dataset')}
+                    showSearch
+                    value={this.state.datasource}
+                  />
+                </div>
+                <div className="dataset-actions">
+                  <Button
+                    buttonStyle="secondary"
+                    onClick={this.openAdvancedSearch}
+                  >
+                    {t('Advanced Search')}
+                  </Button>
+                  <span className="help-text">{datasetHelpText}</span>
+                </div>
               </StyledStepDescription>
             }
           />
@@ -377,6 +414,14 @@ export class ChartCreation extends PureComponent<
             {t('Create new chart')}
           </Button>
         </div>
+        <DatasourceAdvancedSearchModal
+          show={this.state.showAdvancedSearch}
+          onHide={this.closeAdvancedSearch}
+          onDatasourceSelect={this.handleAdvancedSearchSelect}
+          user={this.props.user}
+          addSuccessToast={this.props.addSuccessToast}
+          addDangerToast={this.props.addDangerToast}
+        />
       </StyledContainer>
     );
   }
