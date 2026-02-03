4016104d874562f59e5472c03410cb83f29f4698
Raymond Tieu <8702151+raymondtieu@users.noreply.github.com>
2026-02-03 20:13:22 +0000

    whitelist table and related tags so that styles are not stripped by the xss filter during html sanitization

--- superset-frontend/packages/superset-ui-core/src/utils/html.tsx
+++ superset-frontend/packages/superset-ui-core/src/utils/html.tsx
@@ -25,6 +25,10 @@ const xssFilter = new FilterXSS({
     div: ['style', 'class'],
     a: ['style', 'class', 'href', 'title', 'target'],
     img: ['style', 'class', 'src', 'alt', 'title', 'width', 'height'],
+    table: ['style', 'class'],
+    tr: ['style', 'class'],
+    th: ['style', 'class'],
+    td: ['style', 'class'],
     video: [
       'autoplay',
       'controls',
