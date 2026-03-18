08c3bc30031e55c311c0719f86ef7adb675cb63e
Krishna Gopal <kgopal492@gmail.com>
2026-03-18 10:48:33 -0400

    feat(governance): Add stub file for dashboard list page updates (#159)
    * feat(governance): Add stub file for dashboard list page updates

--- superset-frontend/src/pages/DashboardList/index.tsx
+++ superset-frontend/src/pages/DashboardList/index.tsx
@@ -80,6 +80,13 @@ import { ModifiedInfo } from 'src/components/AuditInfo';
 // @ts-ignore
 // eslint-disable-next-line import/no-unresolved
 import PinterestNewDashboardTierModal from '@pinterest-plugins/src/governance/pinterestNewDashboardTierModal';
+import {
+  getDashboardListExtraColumnsToFetch,
+  getDashboardListExtraListColumns,
+  getDashboardListSearchFilters,
+  // @ts-ignore
+  // eslint-disable-next-line import/no-unresolved
+} from '@pinterest-plugins/src/features/dashboards/dashboardListExtensions';
 
 const PAGE_SIZE = 25;
 const PASSWORDS_NEEDED_MESSAGE = t(
@@ -118,6 +125,10 @@ export interface Dashboard {
   owners: Owner[];
   tags: TagType[];
   created_by: object;
+  /** Governance (internal): tier '1' | '2' | '3' */
+  tier?: string;
+  /** Governance (internal): Nimbus project name */
+  nimbus_project?: string;
 }
 
 const Actions = styled.div`
@@ -179,7 +190,10 @@ function DashboardList(props: DashboardListProps) {
     undefined,
     undefined,
     undefined,
-    DASHBOARD_COLUMNS_TO_FETCH,
+    [
+      ...DASHBOARD_COLUMNS_TO_FETCH,
+      ...(isUserAdmin(reduxUser) ? getDashboardListExtraColumnsToFetch() : []),
+    ],
   );
   const dashboardIds = useMemo(() => dashboards.map(d => d.id), [dashboards]);
   const [saveFavoriteStatus, favoriteStatus] = useFavoriteStatus(
@@ -373,6 +387,7 @@ function DashboardList(props: DashboardListProps) {
         id: 'published',
         className: 'no-ellipsis',
       },
+      ...(isUserAdmin(reduxUser) ? getDashboardListExtraListColumns() : []),
       {
         Cell: ({
           row: {
@@ -639,6 +654,7 @@ function DashboardList(props: DashboardListProps) {
         paginate: true,
         dropdownStyle: { minWidth: WIDER_DROPDOWN_WIDTH },
       },
+      ...(isUserAdmin(reduxUser) ? getDashboardListSearchFilters() : []),
     ] as ListViewFilters;
     return filters_list;
   }, [addDangerToast, favoritesFilter, props.user]);
