4e04c2e88744473aec6ffa3ed700475cdacc69e5
Raymond Tieu <8702151+raymondtieu@users.noreply.github.com>
2026-04-14 21:24:01 -0400

    Surface async websocket errors (#171)

--- superset-frontend/src/dashboard/components/nativeFilters/FilterBar/FilterControls/FilterValue.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterBar/FilterControls/FilterValue.tsx
@@ -358,18 +358,13 @@ const FilterValue: FC<FilterControlProps> = ({
   );
 
   if (error) {
+    const structuredError = error.errors?.[0];
     return (
       <ErrorMessageWithStackTrace
-        error={error.errors?.[0]}
+        error={structuredError}
         compact
-        fallback={
-          <ErrorAlert
-            errorType={t('Network error')}
-            message={t('Network error while attempting to fetch resource')}
-            type="error"
-            compact
-          />
-        }
+        title={t('Error loading filter')}
+        description={structuredError?.message || error.error}
       />
     );
   }
