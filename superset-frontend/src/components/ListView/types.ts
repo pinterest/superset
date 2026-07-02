2c7aaa37fd949f7c218f754da469e35d34034606
Raymond Tieu <8702151+raymondtieu@users.noreply.github.com>
2026-07-02 16:11:18 -0400

    fix(charts): Update default chart list sort to use metric_grade (#353)
    * Update default chart list sort to use metric_grade

--- superset-frontend/src/components/ListView/types.ts
+++ superset-frontend/src/components/ListView/types.ts
@@ -144,8 +144,6 @@ export enum ListViewFilterOperator {
   DashboardIsTier1Candidate = 'dashboard_is_tier1_candidate',
   /** Governance: filter by metric name */
   MetricName = 'metric_name',
-  /** Governance: filter by metric tier */
-  MetricTier = 'metric_tier',
   /** Governance: filter by metric grade */
   MetricGrade = 'metric_grade',
   /** Governance: filter by chart verification status */
